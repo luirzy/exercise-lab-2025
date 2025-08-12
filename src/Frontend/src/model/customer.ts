89eb3f95658a03b383da3e18f1b6c71facbe86c0
luirzy <luigi.vorraro@gmail.com>
2025-08-12 08:18:34 +0000

    Added content inside CustomerListPage

---
+++ src/Frontend/src/model/customer.ts
@@ -0,0 +1,10 @@
+export default interface Customer {
+  id: number;
+  name: string;
+  address: string;
+  email: string;
+  phone: string;
+  iban: string;
+  categoryCode: string;
+  categoryDescription: string;
+}
--- src/Frontend/src/pages/CustomerListPage.tsx
+++ src/Frontend/src/pages/CustomerListPage.tsx
@@ -1,5 +1,77 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+  styled,
+  tableCellClasses,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import Customer from "../model/customer"
+
+
 export default function CustomerListPage() {
-    return <>
-        <i>Todo customers</i>
+  const [list, setList] = useState<Customer[]>([]);
+
+  //this can be moved to customer hook
+  useEffect(() => {
+    fetch("/api/customers/list")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setList(data as Customer[]);
+      });
+  }, []);
+
+  return (
+    <>
+      <Typography variant="h4" sx={{ textAlign: "center", mt: 4, mb: 4 }}>
+        Suppliers
+      </Typography>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <StyledTableHeadCell>Name</StyledTableHeadCell>
+              <StyledTableHeadCell>Address</StyledTableHeadCell>
+              <StyledTableHeadCell>Email</StyledTableHeadCell>
+              <StyledTableHeadCell>Phone</StyledTableHeadCell>
+              <StyledTableHeadCell>Iban</StyledTableHeadCell>
+              <StyledTableHeadCell>Category Code</StyledTableHeadCell>
+              <StyledTableHeadCell>Category Description</StyledTableHeadCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {list.map((customer) => (
+              <TableRow
+                key={customer.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>{customer.name}</TableCell>
+                <TableCell>{customer.address}</TableCell>
+                <TableCell>{customer.email}</TableCell>
+                <TableCell>{customer.phone}</TableCell>
+                <TableCell>{customer.iban}</TableCell>
+                <TableCell>{customer.categoryCode}</TableCell>
+                <TableCell>{customer.categoryDescription}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
+  );
 }
+
+const StyledTableHeadCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.common.white,
+  },
+}));
